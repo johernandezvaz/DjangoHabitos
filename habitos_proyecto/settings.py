852f17c0fe8c5719dafdfc2c87b4e6a9e1399e80
johernandezvaz <johernandezvaz@gmail.com>
2025-05-28 00:32:55 -0600

    Corregir csrf trusted

--- habitos_proyecto/settings.py
+++ habitos_proyecto/settings.py
@@ -15,7 +15,7 @@ from celery.schedules import crontab
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-CSRF_TRUSTED_ORIGINS = ['https://djangohabitos-production.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ["https://djangohabitos-production.up.railway.app"]
 
 
 
