59ddcf9e95b065db4bcf80eb3b7ccc9f0db2a38f
johernandezvaz <johernandezvaz@gmail.com>
2025-05-28 00:11:43 -0600

    modificar requerimientos

--- habitos_proyecto/settings.py
+++ habitos_proyecto/settings.py
@@ -15,6 +15,7 @@ from celery.schedules import crontab
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+CSRF_TRUSTED_ORIGINS = ["*"]
 
 
 # Quick-start development settings - unsuitable for production
